3476c736e7279d4cfb6b0eabb5f7d76a295a60fe
Will Carhart <carhartwill@gmail.com>
2020-02-13 14:33:52 -0800

    Rev koi

--- Formula/koi.rb
+++ Formula/koi.rb
@@ -4,7 +4,7 @@ class Koi < Formula
 	desc "Bashful argument parsing"
 	homepage "https://github.com/wcarhart/koi"
 	url "https://github.com/wcarhart/koi/archive/1.tar.gz"
-	sha256 "5347ca65a1ca6d072c61012f0690a6e079dd8a3283edc06bf359886f61b2f545"
+	sha256 "ce22d05bd1faafac12e22d8897dc83738efd60efd7d1b3992b0dd89e14f95ea6"
 
 	def install
 		bin.install "koi"
