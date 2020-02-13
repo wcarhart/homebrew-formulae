de308a901df5828cf1d33285676074e7ba5494a3
Will Carhart <carhartwill@gmail.com>
2020-02-12 17:08:19 -0800

    Updated koi URL

--- Formula/koi.rb
+++ Formula/koi.rb
@@ -3,7 +3,7 @@ require "formula"
 class Koi < Formula
 	desc "Bashful argument parsing"
 	homepage "https://github.com/wcarhart/koi"
-	url "https://github.com/wcarhart/koi/releases/download/1/koi-1.tar.gz"
+	url "https://github.com/wcarhart/koi/archive/koi-1.tar.gz"
 	sha256 "5347ca65a1ca6d072c61012f0690a6e079dd8a3283edc06bf359886f61b2f545"
 
 	def install
