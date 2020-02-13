c3ba9777a1640f837da146016fd5d05dde2f8124
Will Carhart <carhartwill@gmail.com>
2020-02-13 14:42:20 -0800

    Changed thoth versioning format

--- Formula/thoth.rb
+++ Formula/thoth.rb
@@ -4,8 +4,8 @@ require 'fileutils'
 class Thoth < Formula
 	desc "Helpful English diction tools for the command line"
 	homepage "https://github.com/wcarhart/thoth"
-	url "https://github.com/wcarhart/thoth/archive/1.tar.gz"
-	sha256 "a0df46a945c5046ba57408532c221a11fc039f67298ab175d240ea564ca6541f"
+	url "https://github.com/wcarhart/thoth/archive/v1.tar.gz"
+	sha256 "ca45f67bcd679d33a4f4b62183b04a4d9d02f27ca1e097f2fae0f70f181a4002"
 
 	def install
 		bin.install "thoth"
