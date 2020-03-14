b048dfbaad989bec23964879ad6ee83b1f076c27
Will Carhart <wcarhart@sandiego.edu>
2020-03-14 14:53:54 -0700

    Typo fix

--- Formula/birdhouse.rb
+++ Formula/birdhouse.rb
@@ -3,7 +3,7 @@ require "formula"
 class Birdhouse < Formula
 	desc "Passive tweet watcher from the command line"
 	homepage "https://github.com/wcarhart/birdhouse"
-	url "https://github.com/wcarhart/birdhouse"
+	url "https://github.com/wcarhart/birdhouse/archive/v0.tar.gz"
 	sha256 "f46882a82b263a29b797068ef05713169101d98f184e8f92e8536d41cc0c9745"
 
 	depends_on "wcarhart/tools/koi"
