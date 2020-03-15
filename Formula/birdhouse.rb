07d4621cdcca9d0a63388cca5283e5ff3f7ffde1
Will Carhart <wcarhart@sandiego.edu>
2020-03-15 16:34:56 -0700

    Bump birdhouse

--- Formula/birdhouse.rb
+++ Formula/birdhouse.rb
@@ -3,8 +3,8 @@ require "formula"
 class Birdhouse < Formula
 	desc "Passive tweet watcher from the command line"
 	homepage "https://github.com/wcarhart/birdhouse"
-	url "https://github.com/wcarhart/birdhouse/archive/v1.tar.gz"
-	sha256 "4db03852f98c822eeaa6feb820c6c9c47e8a92c9326f7d3a80046dbbae61e1be"
+	url "https://github.com/wcarhart/birdhouse/archive/v2.tar.gz"
+	sha256 "e890c2022f8bcf754d172987432e6ac21911e5e2cc0e590f9040bcc4ee4d9526"
 
 	depends_on "wcarhart/tools/koi"
 
