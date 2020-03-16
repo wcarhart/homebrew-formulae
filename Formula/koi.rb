d08d94313a67b0457c8869a52a96aee9d1de41a3
Will Carhart <wcarhart@sandiego.edu>
2020-03-15 23:20:02 -0700

    Updated koi hash

--- Formula/koi.rb
+++ Formula/koi.rb
@@ -4,7 +4,7 @@ class Koi < Formula
 	desc "Bashful argument parsing"
 	homepage "https://github.com/wcarhart/koi"
 	url "https://github.com/wcarhart/koi/archive/v11.tar.gz"
-	sha256 "f1e25e80c83dfd1473b8acb6fc59db99b4d28db0aa7d6cf18fcb4db11f41b499"
+	sha256 "8a78a57875397248f2edff5f00578c64c953aee1bc7e140700b374710ac44d42"
 
 	def install
 		bin.install "koi"
