845daf49b673b4865e3810004b9e7bd61341e57e
Will Carhart <wcarhart@sandiego.edu>
2020-03-29 17:26:01 -0700

    Bump koi

--- Formula/koi.rb
+++ Formula/koi.rb
@@ -3,8 +3,8 @@ require "formula"
 class Koi < Formula
 	desc "Bashful argument parsing"
 	homepage "https://github.com/wcarhart/koi"
-	url "https://github.com/wcarhart/koi/archive/v12.tar.gz"
-	sha256 "76b4f0ea814ff6c24d31f6d8af575ee0ba6ebda7705867def6acd32ef9f1abe5"
+	url "https://github.com/wcarhart/koi/archive/v13.tar.gz"
+	sha256 "76c07479ba597f08fa01b013b09f7f010fd93cfec7dbaca98da8164635a6b319"
 
 	def install
 		bin.install "koi"
