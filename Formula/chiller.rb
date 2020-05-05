d82ff811fc08f10651cf871f79e00675ab7ce5fe
Will Carhart <carhartwill@gmail.com>
2020-05-04 18:53:13 -0700

    Bump chiller

--- Formula/chiller.rb
+++ Formula/chiller.rb
@@ -3,8 +3,8 @@ require "formula"
 class Chiller < Formula
 	desc "Helpful GitHub repository and Homebrew tools"
 	homepage "https://github.com/wcarhart/chiller"
-	url "https://github.com/wcarhart/chiller/archive/v1.0.0.tar.gz"
-	sha256 "26942c38286469f2eb4df60c8a293ae15d32bd06a5d48b30901ba33e7410ca33"
+	url "https://github.com/wcarhart/chiller/archive/v1.1.0.tar.gz"
+	sha256 "be587c907625fcd7994253935899985a13372a1650a3fa79940e6e97ae209f94"
 
 	depends_on "wcarhart/tools/koi"
 
