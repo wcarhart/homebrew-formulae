7ca27437c7650f1c2c599f44a8aa08ad5cc4102e
Will Carhart <carhartwill@gmail.com>
2020-05-04 19:11:00 -0700

    Bump kn

--- Formula/kn.rb
+++ Formula/kn.rb
@@ -3,8 +3,8 @@ require "formula"
 class Kn < Formula
 	desc "Config files for safe keeping 😏"
 	homepage "https://github.com/wcarhart/konphig"
-	url "https://github.com/wcarhart/konphig/archive/v1.0.3.tar.gz"
-	sha256 "f601e603934129b6361ac4e954295f45723004ee79ec1473f9191e4593c9738a"
+	url "https://github.com/wcarhart/konphig/archive/v1.0.4.tar.gz"
+	sha256 "59720f400d93ebefdd7dc0ba2f3f25f5a80d1c1c8ec1476b3e7df62afb4ed2af"
 
 	depends_on "wcarhart/tools/koi"
 
