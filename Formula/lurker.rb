a239c188847288531d2cb39d305f82fb5b19b25b
Will Carhart <carhartwill@gmail.com>
2020-05-06 00:07:46 -0700

    Added dependency for lurker

--- Formula/lurker.rb
+++ Formula/lurker.rb
@@ -6,6 +6,8 @@ class Lurker < Formula
 	url "https://github.com/wcarhart/lurker/archive/v1.1.0.tar.gz"
 	sha256 "9cde933d1c5e2810ba7901e0342a9da882b2d84f4d2dcbf2b14fa39ea320a99f"
 
+	depends_on "wcarhart/tools/koi"
+	
 	def install
 		bin.install "lurker"
 	end
