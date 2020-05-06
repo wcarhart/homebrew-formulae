8a43753589aaf476bf00a179f23cf192ac2f6fd0
Will Carhart <carhartwill@gmail.com>
2020-05-06 03:52:16 -0700

    Bump lurker

--- Formula/lurker.rb
+++ Formula/lurker.rb
@@ -3,8 +3,8 @@ require "formula"
 class Lurker < Formula
 	desc "Hacker News terminal client"
 	homepage "https://github.com/wcarhart/lurker"
-	url "https://github.com/wcarhart/lurker/archive/v1.5.0.tar.gz"
-	sha256 "e87d85ada9fd936275d314789d0e30d7f507c74cb206bc7746ab8d35959b2419"
+	url "https://github.com/wcarhart/lurker/archive/v1.6.1.tar.gz"
+	sha256 "9fa6468f67d14a6e93642a87b058ef17b120ce5d78d82c8e64bf178299843fec"
 
 	depends_on "wcarhart/tools/koi"
 	
