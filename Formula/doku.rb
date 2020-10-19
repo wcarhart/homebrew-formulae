3d28e8352ea7182e21007befebd0405cc1930db8
Will Carhart <carhartwill@gmail.com>
2020-10-18 23:55:11 -0700

    Added doku

---
+++ Formula/doku.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Doku < Formula
+	desc "Automated Sudoku solver"
+	homepage "https://github.com/wcarhart/doku"
+	url "https://github.com/wcarhart/doku/archive/1.0.0.tar.gz"
+	sha256 "848085484d9ab30c580b99c22da61966a627e314271185fa313b98cbacc0257d"
+
+	depends_on "deno"
+
+	def install
+		bin.install "doku"
+		bin.install "src/solver.js"
+		bin.install "src/core.js"
+	end
+end
