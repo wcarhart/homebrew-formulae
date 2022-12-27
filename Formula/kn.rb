7809c72206ced6111809428afee3f3930bda3a9b
Will Carhart <carhartwill@gmail.com>
2022-12-27 12:41:51 -0700

    Typo in kn sha

--- Formula/kn.rb
+++ Formula/kn.rb
@@ -4,7 +4,7 @@ class Kn < Formula
 	desc "Config files for safe keeping 😏"
 	homepage "https://github.com/wcarhart/konphig"
 	url "https://github.com/wcarhart/konphig/archive/v1.0.7.tar.gz"
-	sha256 "390a1191c1b925890a417b69f5696c5f0722f64e2b794dcafdf1c1a576eb4d69"
+	sha256 "fd74bbde2ff981fe90e2e765d79041d280cc2481ebe9d6631648de78cc8b07bd"
 
 	depends_on "wcarhart/tools/koi"
 
